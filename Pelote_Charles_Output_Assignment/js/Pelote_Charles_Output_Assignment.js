4bb7b2f68911da74e9d1c4dbbd1ba6e3d33169a5
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-04 19:07:40 -0400

    added more variables
    added more variables and output to the console

--- Pelote_Charles_Output_Assignment/js/Pelote_Charles_Output_Assignment.js
+++ Pelote_Charles_Output_Assignment/js/Pelote_Charles_Output_Assignment.js
@@ -6,10 +6,17 @@
  * Assignment: Output Assignment
  */
 
-var myFirstName = "Charles ";
-var myMiddleInitial = "H ";
-var myLastName = "Pelote ";
-var mySuffix = "II ";
+var myFirstName = "Charles";
+var mySonzFullName = "Charles H Pelote III";
+var myFullName = "Charles H Pelote II";
+var myDadzFullName = "Charles H Pelote Sr.";
+var myOldSuffix = "Jr.";
 
-console.log("My full name is ", myFirstName, myMiddleInitial, myLastName, mySuffix);
-alert("Click OK if you think this assignment was super awesome!!!");
+console.log("My first name is ", myFirstName, ", which is the same as my father and my son.");
+console.log("My full name is ", myFullName);
+console.log("My Dad's full name is ", myDadzFullName);
+console.log("My son's full name is ", mySonzFullName);
+console.log("I changed the suffix of my name from ", myOldSuffix, "to the second.");
+
+
+alert("Click OK if you think this assignment is SUPER AWESOME!!!");
