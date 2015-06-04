c04fb29fc53dfd288b66d932a85608ea5677492d
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-04 19:47:10 -0400

    finishing touches
    added comments and tested for grammar and readabilty

--- Pelote_Charles_Output_Assignment/js/Pelote_Charles_Output_Assignment.js
+++ Pelote_Charles_Output_Assignment/js/Pelote_Charles_Output_Assignment.js
@@ -6,17 +6,29 @@
  * Assignment: Output Assignment
  */
 
-var myFirstName = "Charles";
-var mySonzFullName = "Charles H Pelote III";
-var myFullName = "Charles H Pelote II";
-var myDadzFullName = "Charles H Pelote Sr.";
-var myOldSuffix = "Jr.";
+var myFirstName    = "Charles";                     //assigning a string value
+var mySonzFullName = "Charles H Pelote III";        //assigning a string value
+var myFullName     = "Charles H Pelote II";         //assigning a string value
+var myDadzFullName = "Charles H Pelote Sr.";        //assigning a string value
+var myOldSuffix    = "Jr.";                         //assigning a string value
+var yearsMarried   = 15;                            //assigning a numeric value
+var child1Age      = 12;                            //assigning a numeric value
+var child2Age      = 10;                            //assigning a numeric value
+var child3Age      = 2;                             //assigning a numeric value
+var numberOfKids   = 3;                             //assigning a numeric value
+var truth          = true;                          //assigning a boolean value
+var notTrue        = false;                         //assigning a boolean value
 
-console.log("My first name is ", myFirstName, ", which is the same as my father and my son.");
-console.log("My full name is ", myFullName);
-console.log("My Dad's full name is ", myDadzFullName);
-console.log("My son's full name is ", mySonzFullName);
-console.log("I changed the suffix of my name from ", myOldSuffix, "to the second.");
+console.log("My first name is ", myFirstName, ", which is the same as my father and my son.");    //sending string output to the console with var myFirstName
+console.log("My full name is ", myFullName);                                                      //sending string output to the console with var myFullName
+console.log("My Dad's full name is ", myDadzFullName);                                            //sending string output to the console with var myDadzFullName
+console.log("My son's full name is ", mySonzFullName);                                            //sending string output to the console with var mySonzFullName
+console.log("I changed the suffix of my name from ", myOldSuffix, "to the second some time ago.");//sending string output to the console with var myOldSuffix
+console.log("I have been married ", yearsMarried, "years.");                                      //sending string output to the console with var yearsMarried
+console.log("My wife and I have ", numberOfKids, "kids.");                                        //sending string output to the console with var numberOfKids
+console.log("My son is the oldest at ", child1Age);                                               //sending string output to the console with var child1Age
+console.log("and we have two girls ages ", child2Age, " and ", child3Age);                        //sending string output to the console with var child2Age an child3Age
+console.log("I was once bitten on the leg by a ", numberOfKids, "legged dog");                    //sending string output to the console with var numberOfKids
+console.log("and that is a ", truth, " statement.");                                              //sending string output to the console with var truth
 
-
-alert("Click OK if you think this assignment is SUPER AWESOME!!!");
+alert("Click OK if you think this assignment is SUPER AWESOME!!!");                               //a popup alert with some text
