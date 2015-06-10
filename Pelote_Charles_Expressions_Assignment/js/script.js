53d15120451adac972942f4b5174da90e016c718
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-10 17:52:21 -0400

    triangle
    added triangle to flow

--- Pelote_Charles_Expressions_Assignment/js/script.js
+++ Pelote_Charles_Expressions_Assignment/js/script.js
@@ -6,33 +6,28 @@
  * Assignment: Expressions Assignment
  */
 
-//var operationsArray = ["+", "-", "*", "/"];
-var shape = prompt("Choose one shape: \n1 = semi-circle\n2 = triangel\n3 = trapezoid\n4=square\n5=rectangle");
-var shapeArray = ["semi-circle", "triangle", "trapezoid", "square", "rectangle"];
-
-//var number1 = 0;// = prompt("Enter the first number to " + operation)
-//var number2 = 1;// = prompt("Enter the second number");
-//var number3 = 2;// = prompt("Enter the third number");
-var numbersArray = [0, 0 ,0];
 var answer;
-//alert(operation);
-//alert(number1 + operation + number2);
+var numbersArray = [0, 0 ,0];
+var shapeArray = ["semi-circle", "triangle", "trapezoid", "square", "rectangle"];
+var shape = prompt("Choose one shape: \n1 = semi-circle\n2 = triangle\n3 = trapezoid\n4=square\n5=rectangle");
 
 if (shape == "1") // semi-circle
 {
     var pie = 3.14;
-    var radius = prompt("What is the radius in inches?");
-    numbersArray[0] = radius;
 
-    answer = pie * (Number(numbersArray[0]) * Number(numbersArray[0]))/2; //Number(inputNumbersArray[0]) + Number(inputNumbersArray[1]);
-    //alert(answer);
-    //alert(numbersArray[0]);
-    //alert(Number(numbersArray[0]));
+    numbersArray[shape - 1] = prompt("What is the radius in inches?");
+
+    answer = pie * (Number(numbersArray[shape - 1]) * Number(numbersArray[shape - 1]))/2;
 }
+
 if (shape == "2") // triangle
 {
-    answer = Number(inputNumbersArray[0]) - Number(inputNumbersArray[1]);
+    numbersArray[shape - 1] = prompt("What is the base in inches?");   // base
+    numbersArray[shape]     = prompt("What is the height in inches?"); // height
+
+    answer = Number(numbersArray[shape - 1]) * Number(numbersArray[shape])/2;
 }
+
 if (shape == "3") //trapezoid
 {
     answer = Number(inputNumbersArray[0]) * Number(inputNumbersArray[1]);
@@ -47,4 +42,4 @@ if (shape == "5") //rectangle
 }
 //alert(answer);
 console.log("The area of a " + shapeArray[shape - 1] + " is " + answer + " inches");
-alert("The area of a " + shapeArray[shape - 1] + " is " + answer + " inches");
+alert("The area of a " + shapeArray[shape - 1] + " is " + answer + " inches squared.");
