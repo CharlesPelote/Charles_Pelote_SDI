5e446d06f96eb18f60e8c31ce6ea7b0d7cf7c38b
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-09 21:16:34 -0400

    four operations
    added all four basic operations and the output for two given numbers

--- Pelote_Charles_Expressions_Assignment/js/script.js
+++ Pelote_Charles_Expressions_Assignment/js/script.js
@@ -6,7 +6,29 @@
  * Assignment: Expressions Assignment
  */
 
-var operationsArray = ["+", "-", "*", "/"];
-var operation = prompt("What is the operation?");
+//var operationsArray = ["+", "-", "*", "/"];
+var operation = prompt("Choose one operation: +, -, * or /");
+//var inputNumbersArray = ;
+var number1 = prompt("Enter the first number to " + operation);
+var number2 = prompt("Enter the second number");
 
-alert(operation);
+//alert(operation);
+alert(number1 + operation + number2);
+
+if (operation == "+")
+{
+    var answer = Number(number1) + Number(number2);
+}
+if (operation == "-")
+{
+    var answer = Number(number1) + Number(number2);
+}
+if (operation == "*")
+{
+    var answer = Number(number1) + Number(number2);
+}
+if (operation == "/")
+{
+    var answer = Number(number1) + Number(number2);
+}
+alert(answer);
