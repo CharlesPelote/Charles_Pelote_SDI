4ae83c9639fe43db21db5d9a6d2f87d8219b9c90
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-10 18:35:14 -0400

    completed rectangle
    added rectangle to flow and tested to make sure calculations logic and
    view ability are there.

--- Pelote_Charles_Expressions_Assignment/js/script.js
+++ Pelote_Charles_Expressions_Assignment/js/script.js
@@ -9,30 +9,30 @@
 var answer;
 var numbersArray = [0, 0 ,0];
 var shapeArray = ["semi-circle", "triangle", "trapezoid", "square", "rectangle"];
-var shape = prompt("Choose one shape: \n1 = semi-circle\n2 = triangle\n3 = trapezoid\n4=square\n5=rectangle");
+var shape = prompt("Choose one shape: \n1 = semi-circle\n2 = triangle\n3 = trapezoid\n4 = square\n5 = rectangle");
 
 if (shape == "1") // semi-circle
 {
     var pie = 3.14;
 
-    numbersArray[0] = prompt("What is the radius in inches?");
+    numbersArray[0] = prompt("What is the radius of the semi-circle in inches?");
 
     answer = pie * (Number(numbersArray[shape - 1]) * Number(numbersArray[shape - 1]))/2;
 }
 
 if (shape == "2") // triangle
 {
-    numbersArray[0] = prompt("What is the base in inches?");   // base
-    numbersArray[1] = prompt("What is the height in inches?"); // height
+    numbersArray[0] = prompt("What is the base of the triangle in inches?");   // base
+    numbersArray[1] = prompt("What is the height of the triangle in inches?"); // height
 
     answer = Number(numbersArray[0]) * Number(numbersArray[1])/2;
 }
 
 if (shape == "3") //trapezoid
 {
-    numbersArray[0] = prompt("What is the width of the top in inches?");   // width of the top
-    numbersArray[1] = prompt("What is the width of the bottom in inches?"); // width of the bottom
-    numbersArray[2] = prompt("What is the height in inches?"); // height
+    numbersArray[0] = prompt("What is the width of the top of the trapezoid in inches?");   // width of the top
+    numbersArray[1] = prompt("What is the width of the bottom of the trapezoid in inches?"); // width of the bottom
+    numbersArray[2] = prompt("What is the height of the trapezoid in inches?"); // height
 
     answer = (Number(numbersArray[0]) + Number(numbersArray[1])) * Number(numbersArray[2])/2;
 }
@@ -46,8 +46,11 @@ if (shape == "4") //square
 
 if (shape == "5") //rectangle
 {
-    answer = Number(inputNumbersArray[0]) / Number(inputNumbersArray[1]);
+    numbersArray[0] = prompt("What is the width of the rectangle in inches?"); // width
+    numbersArray[1] = prompt("What is the height of the rectangle in inches?"); // height
+
+    answer = Number(numbersArray[0]) * Number(numbersArray[1]);
 }
-//alert(answer);
+
 console.log("The area of a " + shapeArray[shape - 1] + " is " + answer + " inches");
 alert("The area of a " + shapeArray[shape - 1] + " is " + answer + " inches squared.");
