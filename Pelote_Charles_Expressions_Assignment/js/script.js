d37277f7c3d312e0bb97c76f9a2734f896ea36d7
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-09 20:43:16 -0400

    operation input
    setting up operation input and testing output

--- Pelote_Charles_Expressions_Assignment/js/script.js
+++ Pelote_Charles_Expressions_Assignment/js/script.js
@@ -1,3 +1,12 @@
 /**
- * Created by LoveHateApple on 6/9/15.
+ * Created by Charles Pelote II on 6/9/15.
+ * Student id: 0004666164
+ * Scalable Data Infrastructures
+ * SDI 201506 Section 01
+ * Assignment: Expressions Assignment
  */
+
+var operationsArray = ["+", "-", "*", "/"];
+var operation = prompt("What is the operation?");
+
+alert(operation);
