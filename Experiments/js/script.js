1d5076c6b2f4e8918788e45873b942e465f4cba2
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-09 20:12:54 -0400

    exp
    messing around with stuff

--- Experiments/js/script.js
+++ Experiments/js/script.js
@@ -2,6 +2,17 @@
  * Created by LoveHateApple on 6/6/15.
  */
 
+var number = 2;
+var phrase = 'whats up??';
+var rizo = "howdy doody";
+var muppetNames = ["kermit", "miss piggy", "gonzo", rizo, 45];
+var number = 89;
+var string =
 
-console.log("console.log function");
-/** alert("TEST MSG");
+
+
+
+console.log(phrase);
+console.log(muppetNames[4]);
+
+/** alert("TEST MSG"); **/
