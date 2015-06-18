8c3bbf7aae940e0bb5e1cd77c39535aa9a60d853
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-18 12:44:59 -0400

    prompt validation
    all prompts are validated.

--- Pelote_Charles_Conditionals_Assignment/js/script.js
+++ Pelote_Charles_Conditionals_Assignment/js/script.js
@@ -15,6 +15,17 @@ var shapeArray = ["sphere", "cube", "cylinder", "rectangular prism", "cone", "pa
 var shape = "0";                 // user chooses the shape by picking a number and the choice is saved in variable shape.
 var test  = false;               // test variable used to hold a boolean set to false.
 var pi = 3.14;
+var response;
+/**var userAge = 0;
+var userLastName = " ";
+var userFirstName = " ";
+var userName = " ";
+var userGender = " ";
+
+userAge = prompt("How old are you?");
+userFirstName = prompt("What is your first name?");
+userLastName = prompt("What is your last name?");
+userGender = prompt("Are");**/
 
 do        // this loop will continue to run until the user enters the correct information, numbers 1-7.
 {         // user enters one number to reflect the corresponding shape.
@@ -34,113 +45,158 @@ do        // this loop will continue to run until the user enters the correct in
     }
 } while(test == false);
 
+test = false;
+
 if (shape == "1") // the user chose 1 which is the sphere.  the if statement only does the following code if shape is equal to one.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the diameter of the sphere in inches?")); // takes the users diameter and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
 
     answer = pi * numbersArray[0] * numbersArray[0] * numbersArray[0]/6; // calculates the volume and puts it in answer
 }
+
+test = false;
 
 if (shape == "2") // the user chose 2 which is the cube.  the if statement only does the following code if shape is equal to two.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the length of one side of the cube?"));   // takes the users base and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
 
     answer = numbersArray[0] * numbersArray[0] * numbersArray[0];  // calculates the area and puts it in answer
 }
+
+test = false;
 
 if (shape == "3") // the user chose 3 which is the trapezoid.  the if statement only does the following code if shape is equal to three.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the diameter of the cylinder in inches?"));   // takes the width of the top and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
+
+    test = false;
 
     do
     {
         numbersArray[1] = Number(prompt("What is the height of the cylinder in inches?")); // takes the width of the bottom and puts it in the array
+        (numbersArray[1] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[1] == " ");
+    while(test == false);
 
     answer = pi * numbersArray[0] * numbersArray[0] * numbersArray[1]/4;  // calculates the area and puts it in answer
 }
+
+test = false;
 
 if (shape == "4") // the user chose 4 which is the square.  the if statement only does the following code if shape is equal to four.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the length of the rectangular prism in inches?")); // takes the length of one side of square and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
+
+    test = false;
+
     do
     {
         numbersArray[1] = Number(prompt("What is the width of the rectangular prism in inches?"));
+        (numbersArray[1] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[1] == " ");
+    while(test == false);
+
+    test = false;
+
     do
     {
         numbersArray[2] = Number(prompt("What is the height of the rectangular prism in inches?"));
+        (numbersArray[2] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[2] == " ");
+    while(test == false);
 
     answer = numbersArray[0] * numbersArray[1] * numbersArray[2];  // calculates the area and puts it in answer
 }
+
+test = false;
 
 if (shape == "5") // the user chose 5 which is the rectangle.  the if statement only does the following code if shape is equal to five.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the diameter of the cone in inches?")); // takes the width and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
+
+    test = false;
+
     do
     {
         numbersArray[1] = Number(prompt("What is the height of the cone in inches?")); // takes the height and puts it in the array
+        (numbersArray[1] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[1] == " ");
+    while(test == false);
 
     answer = pi * numbersArray[0] * numbersArray[0] * numbersArray[1]/12;  // calculates the area and puts it in answer
 }
+
+test = false;
 
 if (shape == "6") // the user chose 1 which is the semi-circle.  the if statement only does the following code if shape is equal to one.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the diameter of the parabolic cone in inches?")); // takes the width and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
+
+    test = false;
+
     do
     {
         numbersArray[1] = Number(prompt("What is the height of the parabolic cone in inches?")); // takes the height and puts it in the array
+        (numbersArray[1] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[1] == " ");
+    while(test == false);
 
     answer = 2 * pi * numbersArray[0] * numbersArray[0] * numbersArray[1]/15;  // calculates the area and puts it in answer
 }
+
+test = false;
 
 if (shape == "7") // the user chose 1 which is the semi-circle.  the if statement only does the following code if shape is equal to one.
 {
     do
     {
         numbersArray[0] = Number(prompt("What is the diameter of the elliptical cone in inches?")); // takes the width and puts it in the array
+        (numbersArray[0] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[0] == " ");
+    while(test == false);
+
+    test = false;
+
     do
     {
         numbersArray[1] = Number(prompt("What is the height of the elliptical cone in inches?")); // takes the height and puts it in the array
+        (numbersArray[1] > 0) ? test = true : alert("Please enter a positive number greater than zero.");
     }
-    while(numbersArray[1] == " ");
+    while(test == false);
 
     answer = pi * numbersArray[0] * numbersArray[0] * numbersArray[1]/6;  // calculates the area and puts it in answer
 }
 
-console.log("The volume of a " + shapeArray[shape - 1] + " is " + answer + " inches cubed.");  // outputs a string to the console
-alert("The volume of a " + shapeArray[shape - 1] + " is " + answer + " inches cubed."); // outputs a string to the user
+response = (answer <= 0) ? "Something went wrong." : "The volume of your " + shapeArray[shape - 1] + " is " + answer + " inches cubed.";
+console.log(response);  // outputs a string to the console
+alert(response); // outputs a string to the user
