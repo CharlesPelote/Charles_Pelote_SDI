24256d5da2fa3d13c3f226e3efdc7e250ca12bcb
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-23 17:17:27 -0400

    testing
    sent alert to browser to test connection .. success!!!

--- Pelote_Charles_Functions_Assignment/js/script.js
+++ Pelote_Charles_Functions_Assignment/js/script.js
@@ -1,3 +1,9 @@
 /**
- * Created by LoveHateApple on 6/23/15.
- */
+ * Created by Charles Pelote II on 6/23/15.
+ * Student id: 0004666164
+ * Scalable Data Infrastructures
+ * SDI 201506 Section 01
+ * Assignment: Conditionals Assignment
+ * */
+
+alert("just chcking ");
