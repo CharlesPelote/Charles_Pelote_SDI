66388e097356c90c365806d0435d6d13a626047f
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-23 20:14:31 -0400

    which lottery
    whichLottery() will get the user info on which lottery the user is
    playing and return the name of that lottery

--- Pelote_Charles_Functions_Assignment/js/script.js
+++ Pelote_Charles_Functions_Assignment/js/script.js
@@ -6,4 +6,43 @@
  * Assignment: Conditionals Assignment
  * */
 
-alert("just chcking ");
+// variables
+
+//var myName = prompt("Enter your name:");
+var returnNumber = 0;
+var returnLottery;
+
+// functions
+
+/**function myNameFunction(min, max, number)
+{
+    var randomNumber = Math.random() * (max - min) + min;
+    console.log("You entered " + min + " " + max + " " + randomNumber + ".\n");
+    randomNumber = Math.round(randomNumber);
+    return randomNumber;
+}**/
+
+function whichLottery()
+{
+    var test = true;
+    var lottery;
+    var lotteryArray = ["Powerball", "Florida lottery", "Powerball lottery"];
+
+    while(test == true)
+    {
+        lottery = prompt("Which lottery are you playing? \nChoose from: \n1 = Powerball\n2 = Florida lottery\n3 = Powerball lottery.");
+
+        if(lottery == 1 || lottery == 2 || lottery == 3)
+        {
+            test = false;
+        }
+    }
+    return lotteryArray[lottery - 1];
+}
+//main
+
+//returnNumber = myNameFunction(10, 70, 100);
+returnLottery = whichLottery();
+
+console.log("\nthis is it !!!  " + returnLottery);
+
