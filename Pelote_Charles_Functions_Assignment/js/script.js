01911fe1fad1542b708a4fdba6fa9c49cd239b5f
cpelote <LoveHateApple@Charless-MacBook-Pro.local>
2015-06-24 22:32:16 -0400

    cleaning
    cleaning up the code

--- Pelote_Charles_Functions_Assignment/js/script.js
+++ Pelote_Charles_Functions_Assignment/js/script.js
@@ -17,10 +17,10 @@ var randomCount = 0;
 var magicNumbers = " ";
 var returnLottery;
 var powerBall = 0;
-var number1 = 0;
-var number2 = 0;
-var same = false;
-var tempRandomNumber = 0;
+//var number1 = 0;
+//var number2 = 0;
+//var same = false;
+//var tempRandomNumber = 0;
 
 // FUNCTIONS
 
@@ -42,11 +42,9 @@ function whichLottery()
     return lotteryArray[lottery - 1];
 }  // end whichLottery()
 
-function randomizer(minFL, maxFL)
+function randomizer(minNum, maxNum)
 {
-    var randomFL = Math.round(Math.random() * (maxFL - minFL) + minFL);
-
-    return randomFL;
+    return Math.round(Math.random() * (maxNum - minNum) + minNum);
 } // end floridaLottery()
 
 function areTheSame(num1, num2)
@@ -76,12 +74,9 @@ if(returnLottery == "Florida lottery")
         randomNumberArray[cnt] = randomizer(min, max);
         for(var i = 0; i <= cnt - 1; i++)
         {
-            console.log("i = " + i);
             if(areTheSame(randomNumberArray[cnt], randomNumberArray[i]))
             {
-                console.log("its true!!!!  random = " + randomNumberArray[cnt] + " array = " + randomNumberArray[i] + " at element " + i);
                 randomNumberArray[cnt] = randomizer(min, max);
-                console.log("AGAIN!!!");
                 i = 0;
             }
 
@@ -90,53 +85,6 @@ if(returnLottery == "Florida lottery")
 
         magicNumbers = magicNumbers + " " + randomNumberArray[cnt] + " "; // magicNumbers += " " + etc
     }
-    /**console.log("entering dupt chck loop " + randomNumberArray[0]);
-    for(var i = 0; i <= randomCount - 1; i++)
-    {
-        console.log("in loop 1");
-        for(var k = 0; k <= randomCount - 1; k++)
-        {
-            if(i == k)
-            {
-                k++;
-            }
-            same = areTheSame(randomNumberArray[i], randomNumberArray[k]);
-            if(same == true)
-            {
-                tempRandomNumber = randomizer(min, max);
-                k = randomCount;
-                console.log("temp random " + tempRandomNumber);
-            }
-
-        } i = 0;
-    }
-
-    /**for(var num1 = 0; num1 <= randomCount - 1; num1++)
-    {
-        console.log("L1: lottery number = " + randomNumberArray[num1] + " counter = " + num1);
-        for(var num2 = 0; num2 <= randomCount; num2++)
-        {
-            console.log("a new start");
-            console.log("L2: lottery number l1 = " + randomNumberArray[num1] + " num1 = " + num1 + " lottery number l2= " + randomNumberArray[num2] + " num2 = " + num2);
-            number1 = randomNumberArray[num1];
-            number2 = randomNumberArray[num2];
-            if (num1 == num2)
-            {
-                console.log("\n num2++ " + num2 + "number2 " + number2);
-                num2++;
-                number2 = randomNumberArray[num2];
-                console.log("\n num2++ " + num2 + "number2 " + number2);
-            }
-
-            if (number1 == number2)
-            {
-                 console.log("im in");
-                 randomNumberArray[num1] = randomizer(min, max);
-                 //num1--;
-            }
-
-        }
-    } ***/
     console.log("your magic numbers for the " + returnLottery + " are ...." + " " + magicNumbers);
 }
 
@@ -151,10 +99,19 @@ if(returnLottery == "Powerball lottery")
     for (var cnt = 0; cnt <= randomCount - 1; cnt++)
     {
         randomNumberArray[cnt] = randomizer(min, max);
+        for(var i = 0; i <= cnt - 1; i++)
+        {
+            if(areTheSame(randomNumberArray[cnt], randomNumberArray[i]))
+            {
+                randomNumberArray[cnt] = randomizer(min, max);
+                i = 0;
+            }
+
+        }
         magicNumbers = magicNumbers + " " + randomNumberArray[cnt] + " ";
     }
     console.log("your magic numbers for the " + returnLottery + " are ...." + " " + magicNumbers);
-    powerBall = randomizer(minPB, maxPB)
+    powerBall = randomizer(minPB, maxPB);
     console.log("\n and your Powerball is .... " + powerBall);
 }
 console.log("\nthis is it !!!  " + returnLottery);
